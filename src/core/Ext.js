ec39fd6d766627bfd9452d1696ba58ea8c324ec0
nwhite <nwhite@9390c2fe-9be1-4317-a28e-bdd580ed6029>
2009-07-14 09:20:03 +0000

    [type]:   doc [module]: core [desc]:   Primary documentation for Ext.extend now describes the two argument version. The three argument version is mentioned as an option.
    git-svn-id: http://svn.extjs.com/svn/ext-core/trunk@21 9390c2fe-9be1-4317-a28e-bdd580ed6029

--- src/core/Ext.js
+++ src/core/Ext.js
@@ -167,30 +167,27 @@ Ext.apply = function(o, c, defaults){
         },
 
         /**
-         * Extends one class with another class and optionally overrides members with the passed literal. This class
-         * also adds the function "override()" to the class that can be used to override
-         * members on an instance.
-         * * <p>
-         * This function also supports a 2-argument call in which the subclass's constructor is
-         * not passed as an argument. In this form, the parameters are as follows:</p><p>
-         * <div class="mdetail-params"><ul>
-         * <li><code>superclass</code>
-         * <div class="sub-desc">The class being extended</div></li>
-         * <li><code>overrides</code>
-         * <div class="sub-desc">A literal with members which are copied into the subclass's
-         * prototype, and are therefore shared among all instances of the new class.<p>
-         * This may contain a special member named <tt><b>constructor</b></tt>. This is used
-         * to define the constructor of the new class, and is returned. If this property is
-         * <i>not</i> specified, a constructor is generated and returned which just calls the
-         * superclass's constructor passing on its parameters.</p></div></li>
-         * </ul></div></p><p>
-         * For example, to create a subclass of the Ext GridPanel:
+         * <p>Extends one class to create a subclass and optionally overrides members with the passed literal. This method
+         * also adds the function "override()" to the subclass that can be used to override members of the class.</p>
+         * For example, to create a subclass of Ext GridPanel:
          * <pre><code>
 MyGridPanel = Ext.extend(Ext.grid.GridPanel, {
     constructor: function(config) {
-        // Your preprocessing here
-        MyGridPanel.superclass.constructor.apply(this, arguments);
-        // Your postprocessing here
+
+//      Create configuration for this Grid.
+        var store = new Ext.data.Store({...});
+        var colModel = new Ext.grid.ColumnModel({...});
+
+//      Create a new config object containing our computed properties
+//      *plus* whatever was in the config parameter.
+        config = Ext.apply({
+            store: store,
+            colModel: colModel
+        }, config);
+
+        MyGridPanel.superclass.constructor.call(this, config);
+
+//      Your postprocessing here
     },
 
     yourMethod: function() {
@@ -198,13 +195,25 @@ MyGridPanel = Ext.extend(Ext.grid.GridPanel, {
     }
 });
 </code></pre>
-         * </p>
-         * @param {Function} subclass The class inheriting the functionality
-         * @param {Function} superclass The class being extended
-         * @param {Object} overrides (optional) A literal with members which are copied into the subclass's
-         * prototype, and are therefore shared between all instances of the new class.
+         *
+         * <p>This function also supports a 3-argument call in which the subclass's constructor is
+         * passed as an argument. In this form, the parameters are as follows:</p>
+         * <div class="mdetail-params"><ul>
+         * <li><code>subclass</code> : Function <div class="sub-desc">The subclass constructor.</div></li>
+         * <li><code>superclass</code> : Function <div class="sub-desc">The constructor of class being extended</div></li>
+         * <li><code>overrides</code> : Object <div class="sub-desc">A literal with members which are copied into the subclass's
+         * prototype, and are therefore shared among all instances of the new class.</div></li>
+         * </ul></div>
+         *
+         * @param {Function} subclass The constructor of class being extended.
+         * @param {Object} overrides <p>A literal with members which are copied into the subclass's
+         * prototype, and are therefore shared between all instances of the new class.</p>
+         * <p>This may contain a special member named <tt><b>constructor</b></tt>. This is used
+         * to define the constructor of the new class, and is returned. If this property is
+         * <i>not</i> specified, a constructor is generated and returned which just calls the
+         * superclass's constructor passing on its parameters.</p>
+         * <p><b>It is essential that you call the superclass constructor in any provided constructor. See example code.</b></p>
          * @return {Function} The subclass constructor.
-         * @method extend
          */
         extend : function(){
             // inline overrides
