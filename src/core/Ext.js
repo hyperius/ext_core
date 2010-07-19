65753c10098dc98205a5528deb7edc6486f6e5a4
evant <evant@9390c2fe-9be1-4317-a28e-bdd580ed6029>
2010-07-19 17:05:29 +0000

    [type]:   fix [module]: core [id]:     refs #1032 [desc]:   Fixed an issue regarding compat mode when IE was intranet settings.
    git-svn-id: http://svn.extjs.com/svn/ext-core/branches/ext-3.3.x@240 9390c2fe-9be1-4317-a28e-bdd580ed6029

--- src/core/Ext.js
+++ src/core/Ext.js
@@ -49,6 +49,7 @@ Ext.apply = function(o, c, defaults){
             return r.test(ua);
         },
         DOC = document,
+        docMode = DOC.documentMode,
         isStrict = DOC.compatMode == "CSS1Compat",
         isOpera = check(/opera/),
         isChrome = check(/\bchrome\b/),
@@ -58,8 +59,8 @@ Ext.apply = function(o, c, defaults){
         isSafari3 = isSafari && check(/version\/3/),
         isSafari4 = isSafari && check(/version\/4/),
         isIE = !isOpera && check(/msie/),
-        isIE7 = isIE && check(/msie 7/),
-        isIE8 = isIE && check(/msie 8/),
+        isIE7 = isIE && (check(/msie 7/) || docMode == 7),
+        isIE8 = isIE && (check(/msie 8/) && docMode != 7),
         isIE6 = isIE && !isIE7 && !isIE8,
         isGecko = !isWebKit && check(/gecko/),
         isGecko2 = isGecko && check(/rv:1\.8/),
