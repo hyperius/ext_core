3235b87e1c8f18d7b2f574276806d2ff72c70e6a
evant <evant@9390c2fe-9be1-4317-a28e-bdd580ed6029>
2011-01-18 05:31:53 +0000

    [type]:   fix [module]: core [id]: [desc]:   Change namespace to not use each(), it causes memory leaks in IE
    git-svn-id: http://svn.extjs.com/svn/ext-core/branches/ext-3.3.x@275 9390c2fe-9be1-4317-a28e-bdd580ed6029

--- src/core/Ext.js
+++ src/core/Ext.js
@@ -306,15 +306,29 @@ Company.data.CustomStore = function(config) { ... }
          * @method namespace
          */
         namespace : function(){
-            var o, d;
-            Ext.each(arguments, function(v) {
-                d = v.split(".");
-                o = window[d[0]] = window[d[0]] || {};
-                Ext.each(d.slice(1), function(v2){
-                    o = o[v2] = o[v2] || {};
-                });
-            });
-            return o;
+            var len1 = arguments.length,
+                i = 0,
+                len2,
+                j,
+                main,
+                ns,
+                sub,
+                current;
+                
+            for(; i < len1; ++i) {
+                main = arguments[i];
+                ns = arguments[i].split('.');
+                current = window[ns[0]];
+                if (current === undefined) {
+                    current = window[ns[0]] = {};
+                }
+                sub = ns.slice(1);
+                len2 = sub.length;
+                for(j = 0; j < len2; ++j) {
+                    current = current[sub[j]] = current[sub[j]] || {};
+                }
+            }
+            return current;
         },
 
         /**
