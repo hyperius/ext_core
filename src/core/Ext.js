2d65d3f82520efa71717b1fac179fc1734b4928c
evant <evant@9390c2fe-9be1-4317-a28e-bdd580ed6029>
2014-10-17 02:29:43 +0000

    Fix up gecko for IE11
    git-svn-id: http://svn.extjs.com/svn/ext-core/branches/ext-3.4.x@308 9390c2fe-9be1-4317-a28e-bdd580ed6029

--- src/core/Ext.js
+++ src/core/Ext.js
@@ -66,7 +66,7 @@ Ext.apply = function(o, c, defaults){
         isIE10 = isIE && ((check(/msie 10/) && docMode != 7 && docMode != 8 && docMode != 9) || docMode == 10),
         isIE11 = isIE && ((check(/trident\/7.0/) && docMode != 7 && docMode != 8 && docMode != 9 && docMode != 10) || docMode == 11),
         isIE9m = isIE && (isIE6 || isIE7 || isIE8 || isIE9),
-        isGecko = !isWebKit && check(/gecko/),
+        isGecko = (!isIE && !isWebKit) && check(/gecko/),
         isGecko2 = isGecko && check(/rv:1\.8/),
         isGecko3 = isGecko && check(/rv:1\.9/),
         isBorderBox = isIE9m && !isStrict,
